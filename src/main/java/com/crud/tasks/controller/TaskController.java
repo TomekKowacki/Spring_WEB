edd86a0fda8ba6c70919784e6b9161a51cc95a73
Tomasz Kowacki <tomek.kowacki@gmail.com>
2022-12-04 13:32:36 +0100

    Zmiana struktiry projektu

---
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -0,0 +1,35 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TaskDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/tasks")
+public class TaskController {
+
+    @GetMapping
+    public List<TaskDto> getTasks(){
+        return new ArrayList<>();
+    }
+
+    @GetMapping(value = "{taskId}")
+    public TaskDto getTask(@PathVariable Long taskId){
+        return new TaskDto(1L, "test title", "test_content");
+    }
+
+    @DeleteMapping
+    public void deleteTask(Long id){
+    }
+
+    @PutMapping
+    public TaskDto updateTask(TaskDto taskDto){
+        return new TaskDto(1L,"Edited test title", "Test content");
+    }
+
+    @PostMapping
+    public void createTask(TaskDto taskDto){
+    }
+}
