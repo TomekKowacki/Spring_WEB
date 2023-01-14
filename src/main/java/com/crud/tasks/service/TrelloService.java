92b53246669b38973ec76fa6cfc472a83ae6e97a
Tomasz Kowacki <tomek.kowacki@gmail.com>
2023-01-14 17:23:06 +0100

    23.4 Implementacja maila w projekcie

---
+++ src/main/java/com/crud/tasks/service/TrelloService.java
@@ -0,0 +1,40 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.domain.CreatedTrelloCard;
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static java.util.Optional.ofNullable;
+
+@Service
+@RequiredArgsConstructor
+public class TrelloService {
+
+    private final TrelloClient trelloClient;
+    private final SimpleEmailService emailService;
+    private final AdminConfig adminConfig;
+    private static final String SUBJECT = "Tasks: New Trello card";
+
+    public List<TrelloBoardDto> fetchTrelloBoards() {
+        return trelloClient.getTrelloBoards();
+    }
+
+    public CreatedTrelloCard createTrelloCard(final TrelloCardDto trelloCardDto) {
+        CreatedTrelloCard newCard = trelloClient.createNewCard(trelloCardDto);
+
+        ofNullable(newCard).ifPresent(card -> emailService.send(Mail.builder()
+                .mailTo(adminConfig.getAdminMail())
+                .subject(SUBJECT)
+                .message("New card: \" + trelloCardDto.getName() + \" has been created on your Trello account")
+                .build()
+        ));
+        return newCard;
+    }
+}
---
+++ src/main/java/com/crud/tasks/sheduler/EmailScheduler.java
@@ -0,0 +1,31 @@
+package com.crud.tasks.sheduler;
+
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.service.SimpleEmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class EmailScheduler {
+
+    private static final String SUBJECT = "Tasks: Once a day email";
+    private final SimpleEmailService simpleEmailService;
+    private final TaskRepository taskRepository;
+    private final AdminConfig adminConfig;
+
+    @Scheduled(cron = "0 0 10 * * *")
+    public void sendInformationEmail() {
+        long size = taskRepository.count();
+        simpleEmailService.send(
+                Mail.builder()
+                        .mailTo(adminConfig.getAdminMail())
+                        .subject(SUBJECT)
+                        .message("Currently in database you got: " + size + " tasks")
+                        .build()
+                );
+    }
+}
