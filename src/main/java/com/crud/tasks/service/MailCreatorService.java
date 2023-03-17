e2f40d6d10d838a358ef517eb245f0d02c78cf42
Tomasz Kowacki <tomek.kowacki@gmail.com>
2023-03-17 20:50:26 +0100

    32.3 Szablon maila

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -30,12 +30,31 @@ public class MailCreatorService {
         Context context = new Context();
         context.setVariable("preview_message", "...");
         context.setVariable("message", message);
-        context.setVariable("tasks_url", "http://localhost:8888/crud");
+        context.setVariable("tasks_url", "http://localhost:8080/crud");
         context.setVariable("button", "Visit website");
         context.setVariable("admin_config", adminConfig);
         context.setVariable("goodbye_message", "Bye");
         context.setVariable("show_button", false);
         context.setVariable("is_friend", true);
         return templateEngine.process("mail/created-trello-card-mail", context);
+    }
+
+    public String sendDailyMail(String message) {
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
+        Context context = new Context();
+        context.setVariable("preview_message", "Your daily task preview...");
+        context.setVariable("message", message);
+        context.setVariable("tasks_url", "http://localhost:8080/crud");
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("show_button", true);
+        context.setVariable("is_friend", false);
+        context.setVariable("goodbye_message", "Bye");
+        return templateEngine.process("mail/daily-mail-info", context);
     }
 }
